042168aaaef0650f9e5c4c2b07a5aa2940d0af60
Isaque <isaque.bm9@gmail.com>
2023-09-14 11:10:56 -0300

    Commit inicial com projeto base

---
+++ ITAssetPro/src/main/java/com/example/itassetpro/ItAssetProApplication.java
@@ -0,0 +1,13 @@
+package com.example.itassetpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItAssetProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItAssetProApplication.class, args);
+	}
+
+}
---
+++ ITAssetPro/src/test/java/com/example/itassetpro/ItAssetProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.itassetpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItAssetProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
